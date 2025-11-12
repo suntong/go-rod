40f5cfcd9cfd80b461d0db9ab96a0ec2c711d698
Tong Sun <suntong@cpan.org>
2025-11-12 08:47:22 -0500

    - [!] demo5-submit.go fixed

--- demo5-submit.go
+++ demo5-submit.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/input"
@@ -10,11 +11,16 @@ import (
 
 // This example demonstrates how to fill out and submit a form.
 func main() {
-	page := rod.New().MustConnect().MustPage("https://github.com/search")
+	page := rod.New().
+		MustConnect().
+		Trace(true). // log useful info about what rod is doing
+		Timeout(15 * time.Second).
+		MustPage("https://github.com/search")
 
-	page.MustElement(`input[name=q]`).MustWaitVisible().MustInput("chromedp").MustType(input.Enter)
+	page.MustElement(`input[data-component=input]`).MustWaitVisible().MustInput("rod").MustType(input.Enter)
 
-	res := page.MustElementR("a", "chromedp").MustParent().MustParent().MustNext().MustText()
+	res := page.MustElementR("a", "rod").MustParent().MustParent().MustParent().MustNext().MustText()
 
 	log.Printf("got: `%s`", strings.TrimSpace(res))
+	// got: `A Chrome DevTools Protocol driver for web automation and scraping.`
 }
